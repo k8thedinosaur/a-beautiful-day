83180a09a9034ddf36467edab0bf4e53516248c7
Kate Farkas <kate.m.farkas@gmail.com>
2020-04-03 14:58:41 -0700

    separate bs logic (functions) and ui logic (inputs and actions)and spend a many of time getting all that to cooperate

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,33 @@
 // bs logic 
+var outputArray = [];
+
+function Robogersify(input) {
+  for (var num = 0; num <= input; num++) {
+    if (num.toString().includes("3")) {
+      outputArray.push("Won't you be my neighbor?");
+    } else if (num.toString().includes("2")) {
+      outputArray.push("Boop!");
+    } else if (num.toString().includes("1")) {
+      outputArray.push("Beep!");
+    } else {
+      outputArray.push(num.toString());
+    };
+  };
+  return outputArray;
+};
+
+
+function addCommaSpace(input) {
+  return input.toString().split(",").join(", ");
+};
+
 
 // ui logic
 $(document).ready(function() {
   $("#robot-form").submit(function(event) {
-
-  var number = parseInt($("#number-form").val());
-  // console.log(number);
-
-  var outputArray = [];
-  function Robogersify(input) {
-    for (var num = 0; num <= number; num++) {
-      if (num.toString().includes("3")) {
-        input.push("Won't you be my neighbor?");
-      } else if (num.toString().includes("2")) {
-        input.push("Boop!");
-      } else if (num.toString().includes("1")) {
-        input.push("Beep!");
-      } else {
-        input.push(num.toString());
-      };
-    };
-    return input;
-  };
-
-  var newthing = Robogersify(outputArray);
-  console.log(newthing);
-
-  function addCommaSpace(input) {
-    return input.toString().split(",").join(", ");
-  };
-  var answer = addCommaSpace(outputArray);
-  console.log("makeitpretty: " + answer);
-    
-    
-
+    var number = parseInt($("#number-form").val());
+    Robogersify(number);
+    var answer = addCommaSpace(outputArray);
   $("#output").append(answer);
   event.preventDefault();
   });
