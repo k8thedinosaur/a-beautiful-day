3432a6d0a26757b0b3c579184e9195d230bcf8bb
Kate Farkas <kate.m.farkas@gmail.com>
2020-04-03 11:17:38 -0700

    add functions for 2 and 3 specs

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,26 @@ $(document).ready(function() {
 
     numberArrayToString = numberArray.toString();
     
-    if (numberArrayToString.includes("1")) {
-      console.log("contains 1")
-      var noOnes = numberArrayToString.replace("1", "Beep!");
-      console.log(noOnes);
+    if (numberArrayToString.includes("3")) {
+      var noThrees = numberArrayToString.replace("3", "Won't you be my neighbor?");
     };
     
-    numberArraySplit = numberArrayToString.split(",");
+    if (noThrees.includes("2")) {
+      var noTwos = noThrees.replace("2", "Boop!");
+    }
+
+    if (noTwos.includes("1")) {
+      // console.log("contains 1")
+      var noOnes = noTwos.replace("1", "Beep!");
+      // console.log(noOnes);
+    };
+    
+    // numberArraySplit = numberArrayToString.split(",");
     // console.log(typeof numberArraySplit, " typeof numberArraySplit");
     // console.log(numberArraySplit, "numberArraySplit");
     
 
-    numberArrayJoined = numberArraySplit.join(", ");
+    // numberArrayJoined = numberArraySplit.join(", ");
     
 
     $("#output").append(noOnes);
