656295ff727c0935169a9ca097cf462bfef5547a
Kate Farkas <kate.m.farkas@gmail.com>
2020-04-03 10:12:56 -0700

    add function to display all numbers up to input

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,16 @@ $(document).ready(function() {
   $("#robot-form").submit(function(event) {
 
     var number = parseInt($("#number-form").val());
-    console.log(number);
+    // console.log(number);
 
-    $("#output").append(number);
+    var numberArray = [];
+    for (var num = 0; num <= number; num++) {
+      numberArray.push(num);
+    }
+    
+    console.log(numberArray);
+
+    $("#output").append(numberArray);
     event.preventDefault();
   })
 })
